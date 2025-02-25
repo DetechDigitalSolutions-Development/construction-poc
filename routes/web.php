8b9c233026d2f5e01624060ca0adec7506f55336
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-02-25 18:07:10 +0530

    Route configuration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StaffController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\ReportController;
 
+Route::middleware(['auth'])->group(function () {
+    Route::resource('staff', StaffController::class);
+    Route::resource('attendance', AttendanceController::class);
+    Route::get('attendance/report', [AttendanceController::class, 'generateReport'])->name('attendance.report');
+});
+
+// Default route for the welcome page or homepage
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('attendance/report', [ReportController::class, 'generateAttendanceReport'])->name('attendance.report');
+Route::get('attendance/export', [ReportController::class, 'exportAttendanceReport'])->name('export.attendance');
